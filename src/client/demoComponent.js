42920190aa5f80b491d0ab4f10cedf403f277e3a
madan-webonise <madan.somvanshi@weboniselab.com>
2019-02-20 15:55:48 +0530

    reactstrap installed

--- src/client/demoComponent.js
+++ src/client/demoComponent.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
+import { Button } from 'reactstrap';
 
 export default class Demo extends Component {
   render() {
-    return <div>Welcome to React Boilerplate created successfully...</div>
+    return (
+      <div>
+        Welcome to React Boilerplate created successfully...
+        <Button color="danger">Danger!</Button>
+      </div>
+    )
   }
 }
